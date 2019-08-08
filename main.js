4856adf94c04e086cefe41c136b8784ca8378f08
huxc <455908283@qq.com>
2019-08-08 18:07:08 +0800

    lunbo

--- main.js
+++ main.js
@@ -1,66 +1,71 @@
-let n
-初始化()
-let timer = setInterval(() => {
-    makeLeave(getImage(n))
-        .one('transitionend', (e) => {
-            makeEnter($(e.currentTarget))
-        })
-    makeCurrent(getImage(n + 1))
-    n += 1
+let $buttons = $('#buttons>button')
+let $slides = $('#slides')
+let $images = $slides.children('img')
+let current = 0
+//复制第一张和最后一张
+let $firstCopy = $images.eq(0).clone(true)
+let $lastCopy = $images.eq($images.length - 1).clone(true)
+$slides.append($firstCopy)
+$slides.prepend($lastCopy)
+//初始位置
+$slides.css({ transform: 'translateX(-600px)' })
+//监听buttons
+$('#buttons').on('click', 'button', function (e) {
+    let $button = $(e.currentTarget)
+    let index = $button.index()
+    goToSlide(index)
+})
+//监听controls
+$(previous).on('click', function () {
+    goToSlide(current - 1)
+})
+$(next).on('click', function () {
+    goToSlide(current + 1)
+})
+//图片切换
+function goToSlide(index) {
+    if (index > $buttons.length - 1) {
+        index = 0
+    } else if (index < 0) {
+        index = $buttons.length - 1
+    }
+    if (current === $buttons.length - 1 && index === 0) {
+        // 最后一张到第一张
+        $slides.css({ transform: `translateX(${-($buttons.length + 1) * 600}px)` })
+            .one('transitionend', function () {
+                $slides.hide()
+                $slides.offset()
+                $slides.css({ transform: `translateX(${-(index + 1) * 600}px)` }).show()
+            })
+    } else if (current === 0 && index === $buttons.length - 1) {
+        // 第一张到最后一张
+        $slides.css({ transform: `translateX(0px)` })
+            .one('transitionend', function () {
+                $slides.hide().offset()
+                $slides.css({ transform: `translateX(${-(index + 1) * 600}px)` }).show()
+            })
+    } else {
+        $slides.css({ transform: `translateX(${- (index + 1) * 600}px)` })
+    }
+    current = index
+}
+//设置定时器
+let timer = setInterval(function () {
+    goToSlide(current + 1)
 }, 3000)
-
+$('.allButton,.window').on('mouseenter', function () {
+    window.clearInterval(timer)
+}).on('mouseleave', function () {
+    timer = setInterval(function () {
+        goToSlide(current + 1)
+    }, 3000)
+})
+//监听是否停在目前页面
 document.addEventListener('visibilitychange', function (e) {
     if (document.hidden) {
-        console.log('hidden')
         window.clearInterval(timer)
     } else {
-        console.log('active')
-        timer = setInterval(() => {
-            makeLeave(getImage(n))
-                .one('transitionend', (e) => {
-                    makeEnter($(e.currentTarget))
-                })
-            makeCurrent(getImage(n + 1))
-            n += 1
+        timer = setInterval(function () {
+            goToSlide(current + 1)
         }, 3000)
-    }
-})
-
-
-/*初始化
-$('.images > img:nth-child(1)').addClass('current')
-$('.images > img:nth-child(2)').addClass('enter')
-$('.images > img:nth-child(3)').addClass('enter')
-$('.images > img:nth-child(4)').addClass('enter')*/
-function 初始化() {
-    n = 1
-    $(`.images > img:nth-child(${n})`).addClass('current')
-        .siblings().addClass('enter')
-}
-
-function getImage(n) {
-    return $(`.images > img:nth-child(${x(n)})`)
-}
-
-function makeLeave($node) {
-    return $node.removeClass('current').addClass('leave')
-}
-
-function makeEnter($node) {
-    return $node.removeClass('leave').addClass('enter')
-}
-
-function makeCurrent($node) {
-    return $node.removeClass('enter').addClass('current')
-}
-
-//n求余数
-function x(n) {
-    if (n > 4) {
-        n = n % 4
-        if (n === 0) {
-            n = 4
-        }
-    }
-    return n
-}
+    }})
